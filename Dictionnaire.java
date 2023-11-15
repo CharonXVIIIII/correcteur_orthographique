340a87ad7f40f232944e1abb2f8d8bbc5094db6f
b20008929 <benjamin.beroud@etu.univ-amu.fr>
2023-11-15 09:44:17 +0100

    finit

--- Dictionnaire.java
+++ Dictionnaire.java
@@ -3,15 +3,17 @@ import java.util.HashMap;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
+//import java.util.stream.Collectors;
 
 public class Dictionnaire{
-    HashMap<String, List<String>> dico = new HashMap<>();
+    HashMap<String, String> dico = new HashMap<>();
 
     public Dictionnaire(String file) throws IOException{
+       // List<String> lines = Files.lines(Path.of(file)).collect(Collectors.toList());
         List<String> lines = Files.readAllLines(Paths.get(file));
         for(String line : lines){
             String[] words = line.split(" ");
-            dico.put(words[0], createTrigramme(words[0]));
+            dico.put(words[0], words[0]);
         }
     } 
 
